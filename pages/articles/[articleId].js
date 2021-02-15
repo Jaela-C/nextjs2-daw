ad6b8ae37c3781421e46b00e7c64802c0baf28ee
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-02-15 16:32:22 -0500

    Llamada a la API de forma diÃnámica

--- pages/articles/[articleId].js
+++ pages/articles/[articleId].js
@@ -1,41 +1,67 @@
 import React from 'react';
 
-const ArticleDetails = ( {article} ) => {
-    return (
-        <div>
-            {article.title}
-        </div>
-    );
+const ArticleDetails = ({ article }) => {
+  if (!article) {
+    return "No se pudo obtener el artículo";
+  }
+
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <div>{article.user_data.name}</div>
+      <p>{article.body}</p>
+
+      <Comments articleId={article.id} />
+    </div>
+  );
 };
 
 export default ArticleDetails;
 
 export async function getStaticProps(context) {
+  console.log("context", context);
 
-    console.log( 'context', context )
-    const {articleId} = context.params;
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/${articleId}`)
-    const data = await res.json()
-  
+  try {
+    const { articleId } = context.params;
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/articles/${articleId}`
+    );
+    const data = await res.json();
+
+    console.log("data", data);
+
     if (!data) {
       return {
         notFound: true,
-      }
+      };
     }
-  
+
     return {
       props: {
-          article: data
+        article: data,
       }, // will be passed to the page component as props
-    }
+    };
+  } catch (error) {
+    return {
+      props: {
+        article: null,
+      },
+    };
+  }
 }
 
 export async function getStaticPaths() {
-    return {
-        paths: [
-            { params: { articleId: '1' } },
-            { params: { articleId: '2' } }
-          ],
-      fallback: false // See the "fallback" section below
-    };
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/articles`);
+  const data = await res.json();
+
+  const articles = data.data;
+
+  const paths = articles.map((article) => {
+    return { params: { articleId: "" + article.id } };
+  });
+
+  return {
+    paths,
+    fallback: true, // See the "fallback" section below
+  };
 }
