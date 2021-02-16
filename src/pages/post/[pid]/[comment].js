9095e326ce533d3067f383724156d87380b4241b
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-02-15 22:04:46 -0500

    Comentarios

--- src/pages/post/[pid]/[comment].js
+++ src/pages/post/[pid]/[comment].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import Navigation from '../../../src/components/Navigation'
+import Navigation from '../../../components/Navigation'
 
 const Post = () => {
   const router = useRouter()
